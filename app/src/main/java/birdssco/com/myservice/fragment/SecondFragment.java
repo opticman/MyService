96fd9cae963f93c50b9c5e15ee50239aee4fb7aa
birdssco <backhot99@gmail.com>
2017-09-16 16:01:30 +0700

    Add button controller

--- app/src/main/java/birdssco/com/myservice/fragment/SecondFragment.java
+++ app/src/main/java/birdssco/com/myservice/fragment/SecondFragment.java
@@ -14,35 +14,30 @@ import birdssco.com.myservice.R;
  * Created by opticman on 16-Sep-17.
  */
 
-public class MainFragment extends Fragment{
-
+public class SecondFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
         return view;
-    }   //class create fragment View
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        //  add button controller
-        ButtonController();
 
+        //button controller
+        buttonController();
     }
 
-    private void ButtonController() {
-        Button button = (Button) getView().findViewById(R.id.btnGoToSecond);
+    private void buttonController() {
+        Button button = (Button) getView().findViewById(R.id.btnGoToMain);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction().replace(R.id.contentFramentMain, new SecondFragment())
-                        .addToBackStack(null)
-                        .commit();
-
+                getActivity().getSupportFragmentManager().popBackStack();
             }
         });
     }
-}   //Main Class
+}   //main class
