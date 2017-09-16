20a3ecb055a7686350d56b34ead3ee3b57f63bbf
birdssco <backhot99@gmail.com>
2017-09-16 14:41:04 +0700

    Add fragment

--- app/src/main/java/birdssco/com/myservice/MainActivity.java
+++ app/src/main/java/birdssco/com/myservice/MainActivity.java
@@ -3,11 +3,24 @@ package birdssco.com.myservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import birdssco.com.myservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //add fragment
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFramentMain, new MainFragment())
+                    .commit();
+
+        }
+
+    }   //Main method      savedInstanceState = เก็บค่าต่างๆไปมาของ app
+
+}   //Main class
---
+++ app/src/main/java/birdssco/com/myservice/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package birdssco.com.myservice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import birdssco.com.myservice.R;
+
+/**
+ * Created by opticman on 16-Sep-17.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }   //class create fragment View
+
+}   //Main Class   //
